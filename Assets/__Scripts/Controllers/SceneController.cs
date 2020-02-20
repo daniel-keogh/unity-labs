02927e78e07c28ed269fdfc69e952ab3eba105b1
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-02-20 13:58:05 +0000

    Lab 6
    - Menus & switching between scenes
    - Sound
    - Canvas & buttons, etc.

---
+++ Assets/__Scripts/Controllers/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utilities;
+
+public class SceneController : MonoBehaviour
+{
+    // OnClick Event Handlers
+    public void StartOnClick()
+    {
+        SceneManager.LoadSceneAsync(SceneNames.GAME_LEVEL);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadSceneAsync(SceneNames.MAIN_MENU);
+    }
+
+    public void OptionsOnClick()
+    {
+        SceneManager.LoadSceneAsync(SceneNames.OPTIONS_MENU, LoadSceneMode.Additive);
+    }
+
+    public void OptionsBackOnClicked()
+    {
+        SceneManager.UnloadSceneAsync(SceneNames.OPTIONS_MENU);
+    }
+}
---
+++ Assets/__Scripts/Utilities/SceneNames.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class SceneNames
+    {
+        public const string MAIN_MENU = "MainMenu";
+        public const string OPTIONS_MENU = "OptionsMenu";
+        public const string GAME_LEVEL = "GameScene";
+    }
+}
